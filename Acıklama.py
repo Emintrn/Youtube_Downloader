07c78a5bcb099c0a236fcebbfb36d51ca79c463a
Mehmet Emin Turan <m.eminturan@yahoo.com>
2025-04-16 17:18:52 +0100

    Açıklama eklendi.

---
+++ Acıklama.py
@@ -0,0 +1,4 @@
+# Bu dosya internet üzerinde herkese açık bir kaynaktan alınmıştır.
+# Orijinal kaynağı hatırlamıyorum, yalnızca eğitim ve örnek amaçlı kullanılmaktadır.
+# Kod üzerindeki tüm haklar orijinal yazarına aittir.
+# Bu proje kâr amacı gütmeyen, kişisel bir GitHub paylaşımıdır.
